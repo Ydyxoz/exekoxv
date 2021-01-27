e668daef8ae2569a7f65560ea0a80bf0bb85f50b
Ydyxoz <78032273+Ydyxoz@users.noreply.github.com>
2021-01-27 10:11:35 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+const Client = new Discord.Client;
+
+
+Client.on("ready", () => {
+    console.log("bot opérationnel");
+});
+
+Client.login("process.env.TOKEN");
